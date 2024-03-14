65166b50ec9232cf571c41850fade0789effee60
Digvijay Chauhan <digvijay@quantmhill.com>
2024-03-14 16:15:21 +0530

    update

--- src/ui/topcompanies/slider-card.js
+++ src/ui/topcompanies/slider-card.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Card, Stack, Typography } from "@mui/material";
+import React from 'react';
+import { Box, Card, Stack, Typography } from '@mui/material';
 
 export default function SliderCard({ tittle, partners, image }) {
   return (
@@ -8,33 +8,32 @@ export default function SliderCard({ tittle, partners, image }) {
         p: 2,
         px: 3,
         borderRadius: 8,
-
-        ".logo-img": {
-          height: "50px",
-          width: "50px",
+        '.logo-img': {
+          height: '50px',
+          width: '50px',
         },
-        ".img-box": {
-          textAlign: "center",
+        '.img-box': {
+          textAlign: 'center',
         },
-        ".text-box": {
-          textAlign: "center",
+        '.text-box': {
+          textAlign: 'center',
         },
       }}
     >
       <Stack spacing={2}>
         <Box className="img-box">
           <img
-            src={"/images/topcompanies/sliderimage.jpg" || image}
+            src={'/images/topcompanies/sliderimage.jpg' || image}
             alt="sliderlogo"
             className="logo-img"
           />
         </Box>
         <Box className="text-box">
-          <Typography variant="h5" component={"h5"}>
-            <b> {"MNCs" || tittle}</b>
+          <Typography variant="h5" component={'h5'}>
+            <b> {'MNCs' || tittle}</b>
           </Typography>
-          <Typography variant="p" component={"p"}>
-            {"1.4k+ Companies" || partners}
+          <Typography variant="p" component={'p'}>
+            {'1.4k+ Companies' || partners}
           </Typography>
         </Box>
       </Stack>
